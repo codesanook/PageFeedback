0f22c442018a9a16910344c481e5b8f595094dc6
theeranitp <theeranitp@gmail.com>
2015-03-08 13:07:11 +0700

    add form post to test Rest split word API

---
+++ IcuService/WebContent/split.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+	pageEncoding="utf-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+
+	<form method="post" action="icu">
+		<textarea name="sentence"></textarea>
+		<br /> <br /> 
+		<input type="submit" />
+	</form>
+
+
+</body>
+</html>
--- IcuService/src/com/codesanook/IcuServlet.java
+++ IcuService/src/com/codesanook/IcuServlet.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
 import com.google.gson.Gson;
@@ -13,30 +14,13 @@ import com.ibm.icu.text.BreakIterator;
 
 public class IcuServlet extends HttpServlet {
 
-	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws IOException {
+	private List<String> printEachForward(String source) {
 
-		response.setContentType("text/html;charset=utf-8");
-
-		PrintWriter out = response.getWriter();
-
-		String msg = 
-				"เพราะยูเลยแล้วจะไปโทษใครเพราะการที่เราจะได้เจอใครซักคนที่ถูกใจ อาจจะพึ่งแค่โชคชะตาและพรหมลิขิตไม่ได้ การเปิดใจและเปิดโอกาสให้ตัวเองได้เข้าไปอยู่ในสถานการณ์ที่เหมาะสม ก็ยังเป็นอีกสิ่งหนึ่งที่อาจจะช่วยให้คุณได้เจอความรักที่กำลังเฝ้ารอ เคยมั้ยคะ? เจอ “คนที่ใช่” ในเวลาที่ผิด มันก็น่าเสียใจและน่าเสียดายnoonswoon premium เป็นผู้ช่วยแก้ปัญหาแบบนี้ให้คุณได้ค่ะ เรารู้ว่า ทุกคนมีความชอบที่แตกต่างและหลากหลาย เราขอเป็นคนเพิ่มโอกาสให้คุณ ได้พบกับคนที่อาจจะถูกใจ ด้วยบริการแนะนำคนพิเศษตามสไตล์ที่คุณชอบ มาคุยกันค่ะว่า คุณชอบใคร ที่มีไลฟสไตล์แบบไหน ทัศนคติหรือมุมมองอย่างไร เผื่อว่า ใครอีกคนหนึ่งก็กำลังตามหาคนแบบคุณอยู่เช่นกัน";
 		Locale thaiLocale = new Locale("th");
-		String input = msg;
 		BreakIterator boundary = BreakIterator.getWordInstance(thaiLocale);
-		boundary.setText(input);
-
-		// out.println(printEachForward(boundary, input));
-		// http://www.webub.com/%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%83%E0%B8%8A%E0%B9%89%E0%B8%A0%E0%B8%B2%E0%B8%A9%E0%B8%B2%E0%B9%84%E0%B8%97%E0%B8%A2%20%E0%B8%81%E0%B8%B1%E0%B8%9A%20java%20web%20application%20%20servlet%20%20jsp%20%20JSP-1006-49/
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		String jsonString = gson.toJson(printEachForward(boundary, input));
-		out.println(jsonString);
-	}
-
-	public List<String> printEachForward(BreakIterator boundary, String source) {
-
+		boundary.setText(source);
+		
+		
 		List<String> list = new ArrayList<String>();
 		// StringBuffer strout = new StringBuffer();
 		int start = boundary.first();
@@ -47,8 +31,38 @@ public class IcuServlet extends HttpServlet {
 		}
 
 		return list;
-
 		// return strout.toString();
+	}
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=utf-8");
+
+		PrintWriter out = response.getWriter();
+
+		String msg = "เธเธตเนเนเธญเนเธกเธฃเธฑเธเธเนเธญเธเธเธฒเธขเธเนเธเธฐเธเธฃเธฑเธ";
+
+
+		// out.println(printEachForward(boundary, input));
+		// http://www.webub.com/%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%83%E0%B8%8A%E0%B9%89%E0%B8%A0%E0%B8%B2%E0%B8%A9%E0%B8%B2%E0%B9%84%E0%B8%97%E0%B8%A2%20%E0%B8%81%E0%B8%B1%E0%B8%9A%20java%20web%20application%20%20servlet%20%20jsp%20%20JSP-1006-49/
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String jsonString = gson.toJson(printEachForward(msg));
+		out.println(jsonString);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("application/json;charset=utf-8");
+
+		PrintWriter out = response.getWriter();
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+		String sentence = request.getParameter("sentence");
+		String jsonString = gson.toJson(printEachForward(sentence));
+		out.println(jsonString);
 	}
 
 }
